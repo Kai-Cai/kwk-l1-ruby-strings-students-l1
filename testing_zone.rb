d24d0d3d6b23c3096ded0be49a5a3ffa0a78b114
Kai-Cai <kaiaschiff@gmail.com>
2018-06-05 16:43:41 +0000

    automatically backed up by learn

--- testing_zone.rb
+++ testing_zone.rb
@@ -1,4 +1,24 @@
-#z
+def say_hello
+  your_name = "Charlie"
+  puts "Hello #{your_name}!"
+  her_name = "Randi"
+  puts "Hello #{her_name}!"
+  her_name2 = "Gabriella"
+  puts "Hello #{her_name2}!"
+end
+
+say_hello
+
+
+
+def say_hello(your_name)
+  puts "Hello #{your_name}!"
+end
+
+say_hello("Charlie")
+say_hello("Randi")
+say_hello("Gabriella")
+
 
 def say_hello(your_name, my_name, time_of_day)
   puts "Hello #{your_name}! I'm #{my_name}.  How was your #{time_of_day}"

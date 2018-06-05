0402d0183bbadfe206651d538e4f5c2aa83ed275
Kai-Cai <kaiaschiff@gmail.com>
2018-06-05 16:44:45 +0000

    automatically backed up by learn

--- testing_zone.rb
+++ testing_zone.rb
@@ -1,29 +1,27 @@
-def say_hello
-  your_name = "Charlie"
-  puts "Hello #{your_name}!"
-  her_name = "Randi"
-  puts "Hello #{her_name}!"
-  her_name2 = "Gabriella"
-  puts "Hello #{her_name2}!"
-end
+#def say_hello
+  #your_name = "Charlie"
+  #puts "Hello #{your_name}!"
+  #her_name = "Randi"
+  #puts "Hello #{her_name}!"
+  #her_name2 = "Gabriella"
+  #puts "Hello #{her_name2}!"
+#end
 
-say_hello
+#say_hello
 
 
 
-def say_hello(your_name)
-  puts "Hello #{your_name}!"
-end
+#def say_hello(your_name)
+  #puts "Hello #{your_name}!"
+#end
 
-say_hello("Charlie")
-say_hello("Randi")
-say_hello("Gabriella")
+#say_hello("Charlie")
+#say_hello("Randi")
+#say_hello("Gabriella")
 
 
 def say_hello(your_name, my_name, time_of_day)
   puts "Hello #{your_name}! I'm #{my_name}.  How was your #{time_of_day}"
 end
 
-say_hello2("Charlie")
-say_hello2("Randi")
-say_hello2("Gabriella")
+say_hello("Charlie", "Kaia", "morning")
